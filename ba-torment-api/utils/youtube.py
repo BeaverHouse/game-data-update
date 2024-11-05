f19c3f7ec5d287190a96a106e76f1d4bb8988a63
Austin Lee <haulrest@gmail.com>
2024-11-06 00:57:46 +0900

    241106 Fix alerts (#1)

--- ba-torment-api/utils/youtube.py
+++ ba-torment-api/utils/youtube.py
@@ -3,7 +3,7 @@ import urllib.parse
 from fastapi import HTTPException
 
 def parse_youtube_embed_link(link: str) -> str:
-    if link.startswith("https://youtu.be"):
+    if link.startswith("https://youtu.be/"):
         id = link.split("/")[-1]
     elif link.startswith("https://www.youtube.com/watch?v="):
         id = urllib.parse.parse_qs(urllib.parse.urlparse(link).query)["v"][0]

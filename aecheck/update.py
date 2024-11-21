10dfdddda77ea8fc76c3857a61275f46a4c4960b
Austin Lee <haulrest@gmail.com>
2024-11-21 21:13:18 +0900

    Fix null violation on dungeon (4-star)

--- aecheck/update.py
+++ aecheck/update.py
@@ -51,10 +51,11 @@ def update_character(character: Character):
             if character.is_alter:
                 cur.execute("UPDATE aecheck.characters SET alter_character = %s WHERE character_id = %s AND style IN ('4☆', 'NS')", (f'c{code}', alter_code))
 
-            cur.execute(
-                "INSERT INTO aecheck.dungeon_mappings (character_id, dungeon_id) VALUES (%s, %s)",
-                (character_id, dungeon_id)
-            )
+            if dungeon_id:
+                cur.execute(
+                    "INSERT INTO aecheck.dungeon_mappings (character_id, dungeon_id) VALUES (%s, %s)",
+                    (character_id, dungeon_id)
+                )
 
             for personality in personalities:
                 cur.execute(f"SELECT key FROM aecheck.translations WHERE en = '{personality}'")
@@ -123,23 +124,25 @@ def update_character(character: Character):
 
 if __name__ == "__main__":
     update_character(Character(
-        english_name="Rufus",
+        english_name="Izuna",
         korean_class_name=None,
         style=Style.FOUR.value,
-        altema_url="https://altema.jp/anaden/chara/67",
+        altema_url="https://altema.jp/anaden/chara/1116",
         is_original_4star=True
     ))
     update_character(Character(
-        english_name="Rufus",
-        korean_class_name="블레이즈 히어로",
-        style=Style.AS.value,
-        altema_url="https://altema.jp/anaden/chara/1114",
-    ))
-    update_character(Character(
-        english_name="Shanie",
-        korean_class_name="아크 나이트",
+        english_name="Mighty",
+        korean_class_name="아쿠아 룰러",
         style=Style.NS.value,
         max_manifest=2,
-        alter_character_korean_name="가시나무 저주의 여인 셰이네",
-        altema_url="https://altema.jp/anaden/chara/180",
+        altema_url="https://altema.jp/anaden/chara/3",
+    ))
+    update_character(Character(
+        english_name="Izuna",
+        korean_class_name="카라스텐구",
+        style=Style.NS.value,
+        altema_url="https://altema.jp/anaden/chara/1116",
+        # is_alter=True,
+        # alter_character_korean_name="츠바메",
+        max_manifest=0
     ))

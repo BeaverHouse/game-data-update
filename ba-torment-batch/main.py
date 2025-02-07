6f0518f262e1528b98f25485a08a7051899ea304
Austin Lee <haulrest@gmail.com>
2025-02-07 12:52:01 +0900

    250207 Minor fix 1 (#12)

--- ba-torment-batch/main.py
+++ ba-torment-batch/main.py
@@ -14,13 +14,13 @@ def ba_data_batch() -> None:
     with get_postgres() as conn:
         cur = conn.cursor()
         raid_table = "ba_torment.raids"
-        cur.execute(f"SELECT raid_id FROM {raid_table} WHERE status = 'PENDING' ORDER BY created_at ASC")
-        raids = list(map(lambda x: x[0], cur.fetchall()))
+        cur.execute(f"SELECT raid_id, top_level FROM {raid_table} WHERE status = 'PENDING' ORDER BY created_at ASC")
+        raids = list(map(lambda x: (x[0], x[1]), cur.fetchall()))
 
-        for raid_id in raids:
+        for raid_id, top_level in raids:
             season, target_boss = raid_id.split("-") if "-" in raid_id else (raid_id, 0)
             upload_party_data(season, int(target_boss))
-            upload_summary(season, int(target_boss))
+            upload_summary(season, int(target_boss), top_level)
             cur.execute(f"UPDATE {raid_table} SET status = 'COMPLETE', updated_at = CURRENT_TIMESTAMP WHERE raid_id = '{raid_id}'")
             conn.commit()
 
